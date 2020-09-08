0c9afc593895ce7747d48ab31ea394f5cfcdea85
cassiofb-dev <cassiofb.souza@gmail.com>
2020-09-08 01:10:32 -0300

    fixed array generator

--- js/arrayGen.js
+++ js/arrayGen.js
@@ -1,5 +1,7 @@
 export default str => {
   if(str.match(/[^0-9]/g)) return 'Enter only with numbers';
-  const ARR = new Array(parseInt(str));
-  return ARR.map(x => Math.floor(Math.random() * ARR.length));
+  const SIZE = parseInt(str), ARR = [];
+  if(SIZE > 1000000) return 'Too much elements!';
+  for(let idx = 0; idx < SIZE; idx++) ARR.push(Math.floor(Math.random() * SIZE));
+  return `[${ARR.join(',')}]`;
 }
